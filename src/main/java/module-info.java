b838ffe476292098a01f652102ebacb82238eda6
tugalsan karabacak <tugalsan@gmail.com>
2022-06-21 00:02:26 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.tugalsan.api.file.html.chart {
+    requires com.tugalsan.api.list;
+    requires com.tugalsan.api.time;
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.pack;
+    requires com.tugalsan.api.stream;
+    requires com.tugalsan.api.string;
+    exports com.tugalsan.api.file.html.chart.server;
+}
