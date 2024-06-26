c8b7f2debfd61418b24b9f1a6168b8cc31ab05a5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-26 15:55:18 +0300

    .

--- src/main/java/com/tugalsan/api/file/html/chart/server/TS_FileHtmlChartUtils.java
+++ src/main/java/com/tugalsan/api/file/html/chart/server/TS_FileHtmlChartUtils.java
@@ -14,14 +14,14 @@ public class TS_FileHtmlChartUtils {
     final private static TS_Log d = TS_Log.of(TS_FileHtmlChartUtils.class);
 
     private static void addLine(List<String> strBuffer, String s) {
-        strBuffer.add(TGS_StringUtils.concat(s, "\n"));
+        strBuffer.add(TGS_StringUtils.cmn().concat(s, "\n"));
     }
 
     public static void addHTML_datePicker(List<String> dest, TGS_Time dateFrom, TGS_Time dateTo, List<TGS_Tuple2<String, String>> hidden) {
-        addLine(dest, TGS_StringUtils.concat(
+        addLine(dest, TGS_StringUtils.cmn().concat(
                 "<form method=\"get\" action=\"\">",
-                TGS_StringUtils.concat(TGS_StreamUtils.toLst(
-                        hidden.stream().map(h -> TGS_StringUtils.concat(
+                TGS_StringUtils.cmn().concat(TGS_StreamUtils.toLst(
+                        hidden.stream().map(h -> TGS_StringUtils.cmn().concat(
                         "<input id=\"", h.value0, "\" name=\"", h.value0, "\" type=\"hidden\" value=\"", h.value1, "\">"
                 )))),
                 "From:<input type=\"date\" name=\"dateFrom\" value=\"", dateFrom.toString_YYYY_MM_DD(), "\"> ",
@@ -71,11 +71,11 @@ public class TS_FileHtmlChartUtils {
         IntStream.range(0, charts.size()).forEachOrdered(i -> {
             List<String> destI = TGS_ListUtils.of();
             var c = charts.get(i);
-            addLine(destI, TGS_StringUtils.concat("        function getDataLineChart", String.valueOf(i), "(){"));
+            addLine(destI, TGS_StringUtils.cmn().concat("        function getDataLineChart", String.valueOf(i), "(){"));
             addLine(destI, "            var data = new google.visualization.DataTable();");
             addLine(destI, "            data.addColumn('string', 'X');");
-            addLine(destI, TGS_StringUtils.concat("            data.addColumn('number', '", c.seriNames.get(0), "');"));
-            addLine(destI, TGS_StringUtils.concat("            data.addColumn('number', '", c.seriNames.get(1), "');"));
+            addLine(destI, TGS_StringUtils.cmn().concat("            data.addColumn('number', '", c.seriNames.get(0), "');"));
+            addLine(destI, TGS_StringUtils.cmn().concat("            data.addColumn('number', '", c.seriNames.get(1), "');"));
             addLine(destI, "            data.addRows([");
             IntStream.range(0, c.data.size()).forEachOrdered(di -> {
                 var dataI = c.data.get(di);
@@ -84,26 +84,26 @@ public class TS_FileHtmlChartUtils {
                 d.ci("addHTML_chart", i, di, nem, sic);
                 nem = nem == 0f ? null : nem;
                 sic = sic == 0f ? null : sic;
-                addLine(destI, TGS_StringUtils.concat("                ['", dataI.xLabel, "', ", String.valueOf(nem), ", ", String.valueOf(sic), "]", (di != c.data.size() - 1 ? "," : "")));
+                addLine(destI, TGS_StringUtils.cmn().concat("                ['", dataI.xLabel, "', ", String.valueOf(nem), ", ", String.valueOf(sic), "]", (di != c.data.size() - 1 ? "," : "")));
             });
             addLine(destI, "            ]);");
             addLine(destI, "            return data;");
             addLine(destI, "        }");
-            addLine(destI, TGS_StringUtils.concat("      function getOptionsLineChart", String.valueOf(i), "() {"));
+            addLine(destI, TGS_StringUtils.cmn().concat("      function getOptionsLineChart", String.valueOf(i), "() {"));
             addLine(destI, "        var options = {");
             addLine(destI, "          backgroundColor: '#AAAAAA',");
             addLine(destI, "          chartArea: {width:500,height:120,left:20,top:20},");
             addLine(destI, "          chartArea: {width:'82%', height: '60%', top: '9%', left: '5%', right: '20%', bottom: '%8'},");
             addLine(destI, "          interpolateNulls: true,");
             addLine(destI, "          hAxis: {");
-            addLine(destI, TGS_StringUtils.concat("            title: '", c.xTitle, "',"));
+            addLine(destI, TGS_StringUtils.cmn().concat("            title: '", c.xTitle, "',"));
             addLine(destI, "            slantedText: true,");
             addLine(destI, "            textStyle: {");
             addLine(destI, "               fontSize: 10");
             addLine(destI, "            }");
             addLine(destI, "          },");
             addLine(destI, "          vAxis: {");
-            addLine(destI, TGS_StringUtils.concat("            title: '", c.yTitle, "',"));
+            addLine(destI, TGS_StringUtils.cmn().concat("            title: '", c.yTitle, "',"));
             addLine(destI, "            slantedText: true,");
             addLine(destI, "            textStyle: {");
             addLine(destI, "               fontSize: 10");
@@ -123,7 +123,7 @@ public class TS_FileHtmlChartUtils {
             addLine(destI, "      function getOptionsHistogram" + i + "() {");
             addLine(destI, "        var options = {");
             addLine(destI, "          backgroundColor: '#AAAAAA',");
-            addLine(destI, TGS_StringUtils.concat("          title: '", c.yTitle, "',"));
+            addLine(destI, TGS_StringUtils.cmn().concat("          title: '", c.yTitle, "',"));
             addLine(destI, "          colors: ['#AB0D06', '#007329'],");
             addLine(destI, "          hAxis: {");
             addLine(destI, "            ticks: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]");
@@ -143,8 +143,8 @@ public class TS_FileHtmlChartUtils {
         addLine(strBuffer, "        function drawChart() {");
         IntStream.range(0, charts.size()).forEachOrdered(i -> {
             List<String> destI = TGS_ListUtils.of();
-            addLine(destI, TGS_StringUtils.concat("            drawLineChart('divLineChart", String.valueOf(i), "', getDataLineChart", String.valueOf(i), "(), getOptionsLineChart", String.valueOf(i), "());"));
-            addLine(destI, TGS_StringUtils.concat("            drawHistogram('divHistogram", String.valueOf(i), "', getDataLineChart", String.valueOf(i), "(), getOptionsHistogram", String.valueOf(i), "());"));
+            addLine(destI, TGS_StringUtils.cmn().concat("            drawLineChart('divLineChart", String.valueOf(i), "', getDataLineChart", String.valueOf(i), "(), getOptionsLineChart", String.valueOf(i), "());"));
+            addLine(destI, TGS_StringUtils.cmn().concat("            drawHistogram('divHistogram", String.valueOf(i), "', getDataLineChart", String.valueOf(i), "(), getOptionsHistogram", String.valueOf(i), "());"));
             strBuffer.addAll(destI);
         });
         addLine(strBuffer, "        }");
@@ -153,10 +153,10 @@ public class TS_FileHtmlChartUtils {
         IntStream.range(0, charts.size()).forEachOrdered(i -> {
             List<String> destI = TGS_ListUtils.of();
             addLine(destI, "      <tr>");
-            addLine(destI, TGS_StringUtils.concat("          <td id=\"divLineChart", String.valueOf(i), "\" style=\"width: 900px; height: 250px;\"></td>"));
+            addLine(destI, TGS_StringUtils.cmn().concat("          <td id=\"divLineChart", String.valueOf(i), "\" style=\"width: 900px; height: 250px;\"></td>"));
             addLine(destI, "     </tr>");
             addLine(destI, "      <tr>");
-            addLine(destI, TGS_StringUtils.concat("          <td id=\"divHistogram", String.valueOf(i), "\" style=\"width: 900px; height: 250px;\"></td>"));
+            addLine(destI, TGS_StringUtils.cmn().concat("          <td id=\"divHistogram", String.valueOf(i), "\" style=\"width: 900px; height: 250px;\"></td>"));
             addLine(destI, "     </tr>");
             strBuffer.addAll(destI);
         });
